9249faf21a5ccfc317b2b6d3ccbcda8034ea1567
Cheneba <chenebametuge14@gmail.com>
2025-12-29 21:29:18 +0100

    feat: Add image picker functionality and update dependencies
    - Implemented image picker in profile page to allow users to upload an avatar.
    - Updated app.json to include expo-image-picker plugin with photo permission.
    - Added expo-image-picker and expo-file-system to package.json and package-lock.json.
    - Included base64-arraybuffer as a new dependency.

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,12 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+import * as ImagePicker from 'expo-image-picker';
 
-const Profile = () => {
+const Page = () => {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  }
+
   return (
     <View>
-      <Text>Profile</Text>
+      {image && <Image source={{ uri: image }} style={styles.avatar} />}
+      {!image && <View style={styles.avatar} />}
+      <Button title='Set Avatar Image' onPress={pickImage}></Button>
     </View>
   )
 }
 
-export default Profile
+const styles = StyleSheet.create({
+  avatar: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#ccc',
+    alignSelf: 'center',
+    borderRadius: 100,
+    margin: 40,
+  },
+})
+
+export default Page
