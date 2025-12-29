b25981fc6251fe375a315d10b406cffedd19255e
Cheneba <chenebametuge14@gmail.com>
2025-12-29 18:55:59 +0100

    feat: Enhance layout with authentication sign-out and profile tab
    - Added sign-out button in the header using Ionicons
    - Configured screen options for Tabs with custom styles
    - Included a profile tab with an icon for user navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,15 +1,45 @@
 
+import { supabase } from '@/utils/supabase'
 import { Ionicons }from '@expo/vector-icons'
 import {Tabs} from 'expo-router'
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 
 const Layout = () => {
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+        headerShadowVisible: false,
+        headerStyle: {
+            backgroundColor: '#151515',
+        },
+        headerTintColor: '#fff',
+        tabBarActiveTintColor: '#fff',
+        tabBarStyle: {
+            backgroundColor: '#151515',
+        },
+        headerRight: () => (
+            <TouchableOpacity onPress={() => supabase.auth.signOut()}>
+                <Ionicons 
+                    name='log-out-outline'
+                    size={24}
+                    color='#fff'
+                    style={{marginRight: 16}}
+                />
+            </TouchableOpacity>
+        )
+    }}>
         <Tabs.Screen name='index' options={{
             title: 'Home',
             tabBarIcon: ({size, color}) => (
                 <Ionicons name="home-outline" size={size} color={color} />
+            ),
+        }}
+        />
+
+        <Tabs.Screen name='profile' options={{
+            title: 'Profile',
+            tabBarIcon: ({size, color}) => (
+                <Ionicons name="person-outline" size={size} color={color} />
             ),
         }}
         />
